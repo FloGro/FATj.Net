90ffb641fe983a3a0ff1d94117dbd9a818d97333
FloGro <grogne_f@etna-alternance.net>
2014-10-13 14:51:34 +0200

    commit deux le retour.

--- socket.php
+++ socket.php
@@ -8,9 +8,9 @@ if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0)))
     die("Couldn't create socket: [$errorcode] $errormsg \n");
 }
  
-echo "Socket created";
+echo "Socket created \n";
 
-if(!socket_connect($sock , $ipAddress , 80))
+if(!socket_connect($sock , $ipAddress, 5000))
 {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
@@ -22,4 +22,12 @@ echo "Connection established \n";
 
 return ($sock);
 }
+
+function send($sock, $message) {
+socket_write($sock, $message, strlen($message)) or die("Could not send data to server\n");
+}
+$sock = connect('10.104.30.70');
+$message = 'What ?';
+send($sock, $message);
+socket_close($sock);
 ?>
