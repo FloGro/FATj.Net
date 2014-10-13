b7bab05b47e239a2c95960bd674b7fbf7922c585
FloGro <grogne_f@etna-alternance.net>
2014-10-13 13:08:34 +0200

    first commit

---
+++ socket.php
@@ -0,0 +1,25 @@
+<?php
+function connect($ipAddress) {
+if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0)))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Couldn't create socket: [$errorcode] $errormsg \n");
+}
+ 
+echo "Socket created";
+
+if(!socket_connect($sock , $ipAddress , 80))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Could not connect: [$errorcode] $errormsg \n");
+}
+ 
+echo "Connection established \n";
+
+return ($sock);
+}
+?>
