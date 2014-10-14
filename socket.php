5c9e3f6282ebef35bba7afdcab19291f0a2cd06a
DemisBand <Tom@MacBook-Air-de-Thomas.local>
2014-10-14 09:57:35 +0200

    commuit

--- socket.php
+++ socket.php
@@ -22,4 +22,53 @@ echo "Connection established \n";
 
 return ($sock);
 }
+    
+function connectserver($ipAddress) {
+        if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0)))
+        {
+            $errorcode = socket_last_error();
+            $errormsg = socket_strerror($errorcode);
+            
+            die("Couldn't create socket: [$errorcode] $errormsg \n");
+        }
+        
+        echo "Socket created \n";
+    // Bind the source address
+    if( !socket_bind($sock, $ipAddress , 5000) )
+    {
+        $errorcode = socket_last_error();
+        $errormsg = socket_strerror($errorcode);
+        
+        die("Could not bind socket : [$errorcode] $errormsg \n");
+    }
+    
+    echo "Socket bind OK \n";
+    
+    if(!socket_listen ($sock , 10))
+    {
+        $errorcode = socket_last_error();
+        $errormsg = socket_strerror($errorcode);
+        
+        die("Could not listen on socket : [$errorcode] $errormsg \n");
+    }
+    
+    echo "Socket listen OK \n";
+    
+    echo "Waiting for incoming connections... \n";
+
+    return ($sock);
+}
+
+function transitfile($ipAddress, $output1, $output)
+    {
+    $sock = connect($ipAddress);
+socket_write($sock, $output1, strlen ($output1)) or die("Could not write ttl output\n");
+         sleep(1);
+    socket_write($sock, $output, strlen ($output)) or die("Could not write output\n");
+       
+   
+ 
+        socket_close($sock);
+    }
+    
 ?>
