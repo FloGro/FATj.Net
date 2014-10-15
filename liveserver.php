03cd7b53ca83dc374e98396fa7682e771657d48b
DemisBand <Tom@MacBook-Air-de-Thomas.local>
2014-10-15 10:51:09 +0200

    CO

--- liveserver.php
+++ liveserver.php
@@ -7,14 +7,14 @@
     
     $ipservers = initNetList();
     $ip = getIPs();
-  //start loop to listen for incoming connections
+    //start loop to listen for incoming connections
     
     $sock = connectserver($ip);
-
+    
     
     while (true)
     {
-
+        
         //Accept incoming connection - This is a blocking call
         $client =  socket_accept($sock);
         
@@ -22,21 +22,20 @@
         if(socket_getpeername($client , $address , $port))
         {
             echo "Client $address : $port is now connected to us. \n";
-        
+            
             
             if (!in_array($address, $ipservers))
-                {
-               array_push($ipservers, $address);
-                }
+            {
+                array_push($ipservers, $address);
+            }
             
         }
-         $connect = socket_read($client, 1024) or die("Could not read CONNECT\n");
+        $connect = socket_read($client, 1024) or die("Could not read CONNECT\n");
         if ($connect == "FIN")
         {
-            echo "TEST FIN";
             sleep(1);
             $msg = socket_read($client, 1024) or die("Could not read MESG\n");
-        echo "MESSAGE RETOUR :" . $msg . "\n";
+            echo "MESSAGE RETOUR :" . $msg . "\n";
         }
         else if ($connect != "CO")
         {
@@ -46,40 +45,38 @@
             }
             
             $destip = $connect;
-        //read data from the incoming socket
-            //$destip = socket_read($client, 1024) or die("Could not read input\n");
-        echo $destip;
+            echo $destip;
             $ttl = socket_read($client, 1024) or die("Could not read input\n");
             echo $ttl;
             $ttl = $ttl - 2;
-           $input = socket_read($client, 1024) or die("Could not read input\n");
+            $input = socket_read($client, 1024) or die("Could not read input\n");
             // clean up input string
-           $input = trim($input);
-           echo "Client Message : ".$input."\n";
-        
+            $input = trim($input);
+            echo "Client Message : ".$input."\n";
+            
             $output = $input;
             $output1 = $ttl;
-        $output2 = $destip;
-	if ($output1 <= "0")
-{
-        transitfilec($output2, $output1, $output);
-}
-    else {
-        $nb = rand(0, (sizeof($ipservers) - 1));
-        while ($ipservers[$nb] == $ip)
-        {
-            $nb = rand(0, (sizeof($ipservers) - 1));
+            $output2 = $destip;
+            if ($output1 <= "0")
+            {
+                transitfilec($output2, $output1, $output);
+            }
+            else {
+                $nb = rand(0, (sizeof($ipservers) - 1));
+                while ($ipservers[$nb] == $ip)
+                {
+                    $nb = rand(0, (sizeof($ipservers) - 1));
+                }
+                
+                sleep(1);
+                transitfile($ipservers[$nb], $output2 ,$output1, $output);
+            }
         }
+        sleep(1);
         
-   sleep(1);
-       	transitfile($ipservers[$nb], $output2 ,$output1, $output);
-}
-        }
-sleep(1);
         
-
     }
-socket_close($client);
+    socket_close($client);
     socket_close($sock);
-
-?>
+    
+    ?>
