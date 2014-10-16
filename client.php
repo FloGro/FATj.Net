40289113e61548c4c5a79727230c93b7fcf37905
FloGro <grogne_f@etna-alternance.net>
2014-10-16 19:26:07 +0200

    Update client.php

--- client.php
+++ client.php
@@ -10,7 +10,8 @@ $netList = initNetList();
 $finalDestination = "192.168.118.4";
 //$TTL = $argv[1];
 $TTL = '67';
-$message = file_reader($argv[1], $argv[2]);
+//$message = file_reader($argv[1], $argv[2]);
+$message = ['Paquet','Paquet'];
 
 for($i = 0; $i < sizeof($message); $i++) {
        $server = $netList[rand(0, sizeof($netList) - 1)];
